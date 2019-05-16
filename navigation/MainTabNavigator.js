b73eb8aa67cc2219c50361e6abc0e95bc6a4984d
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-05-16 16:04:35 +0200

    up - 3/4  l  4/4

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -7,10 +7,25 @@ import HomeScreen from '../screens/HomeScreen';
 import ProductsScreen from '../screens/ProductsScreen';
 import SingleProductScreen from '../screens/SingleProductScreen';
 import CartScreen from '../screens/CartScreen';
+import ProfilScreen from '../screens/ProfilScreen';
+import FormScreen from '../screens/FormScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-});
+
+const HomeStack = createStackNavigator(
+   {
+    Home: {
+      screen: HomeScreen,
+    },
+    Login: {
+      screen: LoginScreen,
+    },
+    Signup: {
+      screen: SignupScreen,
+    }
+  },
+);
 
 HomeStack.navigationOptions = {
   tabBarLabel: 'Home',
@@ -24,6 +39,7 @@ HomeStack.navigationOptions = {
       }
     />
   ),
+  barTintColor: '#000',
 };
 
 const ProductsStack = createStackNavigator({
@@ -32,7 +48,7 @@ const ProductsStack = createStackNavigator({
 });
 
 ProductsStack.navigationOptions = {
-  tabBarLabel: 'Products',
+  tabBarLabel: 'Brands',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
@@ -55,8 +71,49 @@ CartStack.navigationOptions = {
   ),
 };
 
+const ProfilStack = createStackNavigator({
+  Profil: ProfilScreen,
+});
+
+ProfilStack.navigationOptions = {
+  tabBarLabel: 'Profil',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-contact${focused ? '' : '-outline'}`
+          : 'md-contact'
+      }
+    />
+  ),
+};
+
+const FormStack = createStackNavigator({
+  Form: FormScreen,
+  SingleProduct: SingleProductScreen,
+});
+
+FormStack.navigationOptions = {
+  tabBarLabel: 'Form',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-list${focused ? '' : '-outline'}` : 'md-list'}
+    />
+  ),
+};
+
 export default createBottomTabNavigator({
   HomeStack,
   ProductsStack,
   CartStack,
+  ProfilStack,
+  FormStack
+},
+{
+  tabBarOptions: {
+    activeTintColor: '#5F1F6D',
+    inactiveTintColor: 'gray',
+  },
 });
